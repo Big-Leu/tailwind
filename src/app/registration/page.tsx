2be434e651bf79ec0d59cd4173abd035ff45a3e5
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-02-14 01:14:17 +0530

    linkage

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Cam from "./cam";
-import eev from "./assets/ev bike.svg"
-import hel from "./assets/hell.svg"
+import Cam from "../cam";
+import eev from "../assets/ev bike.svg"
+import hel from "../assets/hell.svg"
 import Image from "next/image";
 import { NextPage } from "next";
 
