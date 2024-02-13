76e37775f3424f61ed5a2b1d3f197da6529f797b
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-02-14 00:19:03 +0530

    style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Registration from "./registrationpage";
+import Loginpage from "./loginpage";
+import Landing from "./landingpage";
 export default function Home() {
   return (
     <div>
-        <Registration/>
+        <Landing/>
     </div>
   )
 }
