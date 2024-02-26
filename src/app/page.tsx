0606df5e600fd7f944c0fe9092537971f5f51c2a
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-02-27 01:04:38 +0530

    new element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Registration from "./registrationpage";
 import Loginpage from "./loginpage";
 import Landing from "./landingpage";
+import Dashboard from "./dashboard/page";
+import CARD from "./component/card";
 export default function Home() {
   return (
     <div>
-      <Landing />
+      <CARD />
     </div>
   );
 }
