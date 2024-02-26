91cc950f6917f28a4565f0a02b23e638d57e8666
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-02-27 01:36:25 +0530

    day-12-updates-2

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -7,16 +7,25 @@ import Link from "next/link";
 
 const CARD: NextPage = () => {
   return (
-    <div className="w-lvh h-lvh bg-slate-50 flex py-60 justify-center">
-      <div className=" rounded-3xl w-[300px] h-[200px] bg-black flex flex-col  px-2">
-        <div className="flex flex-row font-dangrek text-xl">
+   
+      <div className=" rounded-3xl w-full h-[200px] bg-stone-950 flex flex-col  px-2">
+        <div className="flex flex-row font-dangrek text-stone-500 text-xl">
             <Image className=" max-h-[111px] max-w-[151px] pt-3" src={miniev} alt="" />
             <label className=" mt-12 "> ATHER 450X </label>
         </div>
-        <div className=" rounded-full min-h-0.5 bg-stone-900 max-w-[280px] mb-10"></div>
-        <div className=" "></div>
+        <div className=" rounded-full min-h-0.5 bg-stone-900 max-w-[280px]"></div>
+        <div className="flex flex-row space-x-20 font-dangrek text-sm mt-5 justify-center">
+            <div className="flex flex-col">
+                <label className=" text-emerald-400">MILEGE</label>
+                <label className="">55 KM/L</label>
+            </div>
+            <div className="flex flex-col">
+                <label className="text-emerald-400">HOURLY RENT</label>
+                <label className="">100 RUPEES</label>
+
+            </div>
+        </div>
       </div>
-    </div>
   );
 };
 
