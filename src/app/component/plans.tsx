0d1771ce486d8697d1d081c99cac19c4a9099b23
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-02-28 15:39:20 +0530

    date-11-upadate-1

--- src/app/component/plans.tsx
+++ src/app/component/plans.tsx
@@ -9,14 +9,17 @@ import Link from "next/link";
 
 const PP: NextPage = () => {
   return (
-    <div className="max-w-[500px] bg-inherit flex justify-center pt-32">
-      <div className="w-full min-h-[580px] static z-20 hover:shadow-md rounded-2xl bg-stone-900 overflow-visible ">
-        <div className=" relative z-30 left-[-4rem] top-[1rem] rounded-tl-lg rounded-tr-lg rotate-[315deg] transform-gpu text-center max-w-[200px]  bg-emerald-400 py-1">
-          <label className=" relative text-white z-40 rotate-[315deg] font-dangrek px-4 text-4xl">
+    <div className=" static max-w-[500px] bg-inherit flex justify-center pt-32">
+      <div className=" left-[2.2rem] top-[13.4rem] min-w-[100px] min-h-[40px] rounded-lg rotate-[-45deg]  bg-emerald-500 z-10"></div>
+      <div className=" relative min-w-full min-h-[600px] z-20 hover:shadow-md rounded-2xl bg-stone-900 overflow-visible ">
+        <div className="absolute top-[1rem] left-[-4rem]  z-30 rounded-tl-lg rounded-bl-lg rounded-br-lg rounded-tr-lg rotate-[315deg] transform-gpu text-center min-w-[230px]  bg-emerald-400 py-3">
+          <label className=" relative text-white z-40 rotate-[315deg]  font-dangrek px-6  text-3xl">
             12 HOURS
           </label>
+          {/* <div className="relative w-[70px] h-[40px]  rounded-bl-lg mt-[5.32rem] left-[-23.1rem] rotate-[-45deg] transform-gpu bg-emerald-500 "></div>
+          <div className="relative w-[70px] h-[40px] rotate-[313deg] rounded-br-lg ml-[-18rem] mt-[-9rem] transform-gpu bg-emerald-500 z-10 "></div> */}
         </div>
-        <div  className=" relative w-full flex justify-center pt-10">
+        <div className=" relative w-full flex justify-center pt-10">
           <div className=" relative items-center z-20 w-[132px] h-[132px] bg-emerald-400 rounded-full flex flex-row justify-center space-x-1">
             <div>
               <Image className="z-30" src={ru} alt="" />
@@ -28,39 +31,73 @@ const PP: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className="w-full h-full relative top-[7rem] flex flex-col mt-[-4.4rem] space-y-3 px-5">
+        <div className="w-full h-full relative top-[7rem] flex flex-col mt-[-4.4rem] space-y-3 px-5 ml-10">
           <div className=" w-full flex flex-row space-x-3">
-            <Image className="z-30  object-cover max-w-5 max-h-5 mt-1" src={check} alt="" />
-            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">Neque porro quisquam est qui dolorem</label>
+            <Image
+              className="z-30  object-cover max-w-5 max-h-5 mt-1"
+              src={check}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
+          </div>
+          <div className="flex flex-row space-x-3 ">
+            <Image
+              className="z-30 object-cover max-w-5 max-h-5 mt-1"
+              src={cross}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
           </div>
           <div className="flex flex-row space-x-3">
-            <Image className="z-30 object-cover max-w-5 max-h-5 mt-1" src={cross} alt="" />
-            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">Neque porro quisquam est qui dolorem</label>
+            <Image
+              className="z-30 object-cover max-w-5 max-h-5 mt-1"
+              src={cross}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
           </div>
           <div className="flex flex-row space-x-3">
-            <Image className="z-30 object-cover max-w-5 max-h-5 mt-1" src={cross} alt="" />
-            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">Neque porro quisquam est qui dolorem</label>
+            <Image
+              className="z-30 object-cover max-w-5 max-h-5 mt-1"
+              src={check}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
           </div>
           <div className="flex flex-row space-x-3">
-            <Image className="z-30 object-cover max-w-5 max-h-5 mt-1" src={check} alt="" />
-            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">Neque porro quisquam est qui dolorem</label>
+            <Image
+              className="z-30 object-cover max-w-5 max-h-5 mt-1"
+              src={check}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
           </div>
           <div className="flex flex-row space-x-3">
-            <Image className="z-30 object-cover max-w-5 max-h-5 mt-1" src={check} alt="" />
-            <label className=" font-dangrek text-xl/2 text-emerald-400 mt-1">Neque porro quisquam est qui dolorem</label>
+            <Image
+              className="z-30 object-cover max-w-5 max-h-5 mt-1"
+              src={cross}
+              alt=""
+            />
+            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">
+              Neque porro quisquam est qui dolorem
+            </label>
           </div>
-          <div className="flex flex-row space-x-3">
-            <Image className="z-30 object-cover max-w-5 max-h-5 mt-1" src={cross} alt="" />
-            <label className=" font-dangrek text-xl/2 text-stone-500 mt-1">Neque porro quisquam est qui dolorem</label>
-          </div>
-          <div className=" relative ">
-            <button className=" relative py-2 px-3 rounded-lg text-xl font-dangrek bg-emerald-400">BOOK NOW</button>
+          <div className=" relative pt-10">
+            <button className=" absolute left-[5rem] py-2 px-3 rounded-lg text-xl font-dangrek bg-emerald-400">
+              BOOK NOW
+            </button>
           </div>
         </div>
-      </div>
-      <div className=" relative z-10 ">
-        <div className="relative w-[70px] h-[40px]  rounded-bl-lg mt-[5.32rem] left-[-23.1rem] rotate-[-45deg] transform-gpu bg-emerald-500 "></div>
-        <div className="relative w-[70px] h-[40px] rotate-[313deg] rounded-br-lg ml-[-18rem] mt-[-9rem] transform-gpu bg-emerald-500 z-10 "></div>
       </div>
     </div>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,74 +3,81 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { NextPage } from "next";
 import Link from "next/link";
-import evlogo from "../assets/ev bike.svg"
-import acc from "../assets/Account.svg"
-import dash from "../assets/svd.svg"
-import station from "../assets/location.svg"
-import maps from "../assets/maaps.svg"
-import sub from "../assets/subp.svg"
+import evlogo from "../assets/ev bike.svg";
+import acc from "../assets/Account.svg";
+import dash from "../assets/svd.svg";
+import station from "../assets/location.svg";
+import maps from "../assets/maaps.svg";
+import sub from "../assets/subp.svg";
 import CARD from "../component/card";
-import profile from "../assets/profile.svg"
-import Slider from 'react-slick'
+import profile from "../assets/profile.svg";
+import Slider from "react-slick";
 import PLAN from "../component/plan";
 import PP from "../component/plans";
 const Dashboard: NextPage = () => {
-
   return (
     <div className="w-lvh h-lvh bg-transparent flex flex-row justify-start">
-       <div className="min-w-[400px] h-full bg-stone-900 flex flex-col space-y-10 ">
-          <div className="pt-10 ml-10">
+      <div className="min-w-[300px] h-full bg-stone-900 flex flex-col space-y-10 ">
+        <div className="pt-10 ml-10">
           <Image className=" object-cover " src={evlogo} alt="" />
+        </div>
+        <div className="flex flex-col space-y-6">
+          <div className="flex flex-row hover:text-emerald-400 font-dangrek text-xl px-10 justify-stretch">
+            <Image className=" object-cover " src={dash} alt="" />
+            <h2 className="ml-5">DASHBOARD</h2>
           </div>
-          <div className="flex flex-col space-y-6">
-              <div className="flex flex-row hover:text-emerald-400 font-dangrek text-xl px-10 justify-stretch">
-                <Image className=" object-cover " src={dash} alt="" />
-                <h2 className="ml-5">DASHBOARD</h2>
-              </div>
-              <div className="flex flex-row font-dangrek hover:text-emerald-400 text-xl px-10 justify-stretch">
-                <Image className=" object-cover " src={station} alt="" />
-                <h2 className="pl-5 pt-1">STATIONS</h2>
-              </div>
-              <div className="flex flex-row font-dangrek  hover:text-emerald-400 text-xl px-10 justify-stretch">
-                <Image className=" object-cover " src={maps} alt="" />
-                <h2 className="pl-5">MY TRIPS</h2>
-                </div>
-              <div className="flex flex-row font-dangrek text-xl px-10 hover:text-emerald-400 justify-stretch">
-                <Image className=" object-cover " src={acc} alt="" />
-                <h2 className="pl-5">ACCOUNT</h2>
-              </div>
-              <div className="flex flex-row font-dangrek text-xl hover:text-emerald-400 px-10 justify-stretch">
-                <Image className=" object-cover " src={sub} alt="" />
-                <h2 className="pl-5">SUBCRIPTION PLAN</h2>
-              </div>
+          <div className="flex flex-row font-dangrek hover:text-emerald-400 text-xl px-10 justify-stretch">
+            <Image className=" object-cover " src={station} alt="" />
+            <h2 className="pl-5 pt-1">STATIONS</h2>
           </div>
-          <div className=" font-dangrek text-2xl text-center">
-                <h1>AVAILABLE VEHICLES</h1>
-           </div>
-           <div className=" px-5 space-y-3 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-emerald-400 scrollbar-track-stone-900 overflow-y-auto">
-               <CARD />
-               <CARD />
-           </div>
-       </div>
-       <div className=" relative flex flex-col w-full h-full bg-stone-950 text-center">
-           <div className="flex flex-row">
-               <label className=" font-dangrek text-emerald-400 text-xl absolute left-10 top-8">DASHBOARD</label>
-               <div className=" absolute right-4 top-5 space-x-4 flex flex-row">
-                 <div className=" flex flex-col">
-                    <label className=" font-dangrek text-2xl text-emerald-400">Account</label>
-                    <label className=" font-dangrek text-sm">abc@gmail.com</label>
-                 </div>
-                 <Image className=" object-cover " src={profile} alt="" />
-               </div>
-           </div>
-           <div className="ml-14 pt-20 ">
-            <div className="grid grid-cols-3 place-items-stretch">
-                <PP/>
-                <PP/>
-                <PP/>
+          <div className="flex flex-row font-dangrek  hover:text-emerald-400 text-xl px-10 justify-stretch">
+            <Image className=" object-cover " src={maps} alt="" />
+            <h2 className="pl-5">MY TRIPS</h2>
+          </div>
+          <div className="flex flex-row font-dangrek text-xl px-10 hover:text-emerald-400 justify-stretch">
+            <Image className=" object-cover " src={acc} alt="" />
+            <h2 className="pl-5">ACCOUNT</h2>
+          </div>
+          <div className="flex flex-row font-dangrek text-xl hover:text-emerald-400 px-10 justify-stretch">
+            <Image className=" object-cover " src={sub} alt="" />
+            <h2 className="pl-5">SUBCRIPTION PLAN</h2>
+          </div>
+        </div>
+        <div className=" font-dangrek text-2xl text-center">
+          <h1>AVAILABLE VEHICLES</h1>
+        </div>
+        <div className=" px-5 space-y-3 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-emerald-400 scrollbar-track-stone-900 overflow-y-auto">
+          <CARD />
+          <CARD />
+        </div>
+      </div>
+      <div className=" relative flex flex-col w-full h-full bg-stone-950 text-center">
+        <div className="flex flex-row">
+          <label className=" font-dangrek text-emerald-400 text-xl absolute left-10 top-8">
+            DASHBOARD
+          </label>
+          <div className=" absolute right-4 top-5 space-x-4 flex flex-row">
+            <div className=" flex flex-col">
+              <label className=" font-dangrek text-2xl text-emerald-400">
+                Account
+              </label>
+              <label className=" font-dangrek text-sm">abc@gmail.com</label>
             </div>
-           </div>
-       </div>
+            <Image className=" object-cover " src={profile} alt="" />
+          </div>
+        </div>
+        <div className="pl-10  h-full flex flex-row gap-12 pt-10 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-emerald-400 scrollbar-track-stone-900 overflow-y-auto">
+          <div>
+            <PP />
+          </div>
+          <div>
+            <PP />
+          </div>
+          <div>
+            <PP />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
