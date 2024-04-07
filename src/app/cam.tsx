63560be0aed64ce1195730056cd99689d462789b
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-04-07 23:27:43 +0530

    date-07/04/2023-update-1

--- src/app/cam.tsx
+++ src/app/cam.tsx
@@ -23,27 +23,27 @@ const Cam: React.FC = () => {
   return (
     <>
       <header>
-        <h1 className="relative font-normal font-dangrek text-[30px] top-[30px] text-emerald-400">Image Capture</h1>
+        <h1 className="relative font-normal font-dangrek text-4xl mt-[2rem] text-emerald-400">Image Capture</h1>
       </header>
       {!isCaptureEnable && (
-        <button className=" relative left-[1000px] w-[114px] top-[-10px] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-xl font-dangrek py-2 px-4 rounded" onClick={() => setCaptureEnable(true)}>Start</button>
+        <button className="absolute ml-[48rem] mt-[-2.3rem] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-3xl font-dangrek py-[0.1rem] px-[.5rem] rounded" onClick={() => setCaptureEnable(true)}>Start</button>
       )}
       {isCaptureEnable && (
         <>
           <div>
-            <button className="relative left-[1000px] w-[114px] top-[-10px] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-xl font-dangrek py-2 px-4 rounded"onClick={() => setCaptureEnable(false)}>End</button>
+            <button className="absolute mt-[32.5rem] ml-[1rem] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-3xl font-dangrek py-[0.1rem] px-[1rem] rounded" onClick={() => setCaptureEnable(false)}>End</button>
           </div>
-          <div className="rounded-md border-double border-4 w-[540px] border-emerald-400">
+          <div className="rounded-md mt-10 mb-10 border-double border-[1vw] border-emerald-400">
             <Webcam
               audio={false}
-              width={540}
-              height={360}
+              width="100%"
+              height="auto"
               ref={webcamRef}
               screenshotFormat="image/jpeg"
               videoConstraints={videoConstraints}
             />
           </div>
-          <button className="relative top-[-330px] left-[850px] w-[114px] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-xl font-dangrek py-2 px-4 rounded" onClick={capture}>Capture</button>
+          <button className="absolute mt-[-1rem] ml-[8rem] bg-emerald-400 hover:bg-emerald-500 text-white font-normal text-3xl font-dangrek py-[0.1rem] px-[1rem] rounded" onClick={capture}>Capture</button>
         </>
       )}
       {url && (
@@ -52,12 +52,12 @@ const Cam: React.FC = () => {
             <button onClick={() => setUrl(null)}>Delete</button>
           </div>
           <div>
-            <Image src={url} alt="Screenshot" width="540" height="360"/>
+            <Image src={url} alt="Screenshot" width="100%" height="auto"/>
           </div>
         </>
       )}
     </>
-  );
+  )
 };
 
 export default Cam;
