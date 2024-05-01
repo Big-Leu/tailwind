65526e9e147b3a495eefc14fcb4de5730ebf0fbe
Big-Leu <100833208+Big-Leu@users.noreply.github.com>
2024-05-02 01:24:04 +0530

    main update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['bucket21.s3.amazonaws.com','newface21.s3.amazonaws.com'],
+        domains: ['bucket21.s3.amazonaws.com','newface21.s3.amazonaws.com','E:/ganesh kv-ml/tailwind/src/app/assets/w1.png'],
       },
 };
 
--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -2,6 +2,10 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import miniev from "../assets/mini.svg"
+import w1 from "../assets/w1.png"
+import w2 from "../assets/w2.png"
+import w3 from "../assets/w33.png"
+
 import { NextPage } from "next";
 import Link from "next/link";
 
@@ -9,6 +13,7 @@ interface WelcomeProps {
 name:string;
 milage :string;
 Price:string;
+vehicle:string;
   }
 
 const CARD: NextPage<WelcomeProps> = (props) => {
@@ -16,13 +21,13 @@ const CARD: NextPage<WelcomeProps> = (props) => {
    
       <div className=" rounded-3xl w-full h-[200px] bg-stone-950 flex flex-col  px-2">
         <div className="flex flex-row font-dangrek text-stone-500 text-xl">
-            <Image className=" max-h-[111px] max-w-[151px] pt-3" src={miniev} alt="" />
+            <Image className=" max-h-[111px] max-w-[151px] pt-3" src={props.vehicle === "miniev" ? miniev : props.vehicle === "w1" ? w1 : props.vehicle === "w2" ? w2 : w3} alt="" width="100" height="60"/>
             <label className=" mt-12 "> {props.name} </label>
         </div>
         <div className=" rounded-full min-h-0.5 bg-stone-900 max-w-[280px]"></div>
         <div className="flex flex-row space-x-20 font-dangrek text-sm mt-5 justify-center">
             <div className="flex flex-col">
-                <label className=" text-emerald-400">MILEGE</label>
+                <label className=" text-emerald-400">MILAGE</label>
                 <label className="">{props.milage}</label>
             </div>
             <div className="flex flex-col">
