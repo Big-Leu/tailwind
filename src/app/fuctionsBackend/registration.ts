f84950dd68c89cae7069a71bda2c74543e56e546
Big-Leu <abhishek.rawat21b@iiitg.ac.in>
2024-08-06 13:09:14 +0530

    "minor update of rigisration form"

---
+++ src/app/fuctionsBackend/registration.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const handleLoginGoogle = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  e.preventDefault(); // Prevent default action
+
+  try {
+    const response = await fetch('http://localhost:8000/api/v1/auth/google/authorize', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const authorizationUrl = data.authorization_url;
+    if (authorizationUrl) {
+      window.location.href = authorizationUrl;
+      console.log('Redirecting to:', authorizationUrl);
+    } else {
+      throw new Error('Authorization URL not found in response');
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+  }
+};
--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -13,8 +13,8 @@ const Registration: NextPage= () => {
  const [email, setEmail] = useState("");
  const [password, setPassword] = useState("");
  const [phone, setPhone] = useState("");
- const [company, setCompany] = useState("");
- const [photo, setPhoto] = useState(null);
+//  const [company, setCompany] = useState("");
+//  const [photo, setPhoto] = useState(null);
 
  // Function to handle registration
  const handleRegister = async () => {
@@ -23,7 +23,6 @@ const Registration: NextPage= () => {
      const formData = {
        firstName: firstName,
        lastName: lastName,
-       work: company,
        mobile: phone,
        email: email,
        password: password
@@ -84,7 +83,7 @@ const Registration: NextPage= () => {
                onChange={(e) => setEmail(e.target.value)} 
                value={email}
                required
-               type="email" name="floating_email" id="floating_email" className="h-20 block py-2.5 px-0 w-full text-xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+               type="email" name="floating_email" id="floating_email" className="h-20 block py-2.5 px-0 w-full text-xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                 <label form="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
               </div>
               <div className="relative z-0 w-full h-20 mb-5 group">
@@ -92,7 +91,7 @@ const Registration: NextPage= () => {
                 onChange={(e) => setPassword(e.target.value)} 
                 value={password}
                 required
-                type="password" name="floating_password" id="floating_password" className="h-20 block py-2.5 px-0 w-full  text-xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                type="password" name="floating_password" id="floating_password" className="h-20 block py-2.5 px-0 w-full  text-xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                 <label form="floating_password" className="peer-focus:font-meduim absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
               </div>
               <div className="relative z-0 w-full h-20 mb-5 group">
@@ -100,7 +99,7 @@ const Registration: NextPage= () => {
                 onChange={(e) => setPassword(e.target.value)} 
                 value={password}
                 required
-                type="password" name="repeat_password" id="floating_repeat_password" className="block h-20 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                type="password" name="repeat_password" id="floating_repeat_password" className="block h-20 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                 <label form="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
               </div>
               <div className="grid xl:gap-10 md:grid-cols-2 h-20 md:gap-6">
@@ -109,34 +108,26 @@ const Registration: NextPage= () => {
                   onChange={(e) => setFirstName(e.target.value)} 
                   value={firstName}
                   required                  
-                  type="text" name="floating_first_name" id="floating_first_name" className=" h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                  <label form="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+                  type="text" name="floating_first_name" id="floating_first_name" className=" h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+                  <label form="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">FirstName</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group h-20">
                   <input 
                   onChange={(e) => setLastName(e.target.value)} 
                   value={lastName}
                   required                  
-                  type="text" name="floating_last_name" id="floating_last_name" className="h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                  <label form="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
+                  type="text" name="floating_last_name" id="floating_last_name" className="h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+                  <label form="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">LastName</label>
                 </div>
               </div>
-              <div className="pt-10 grid  md:grid-cols-2 md:gap-6 h-20">
+              <div className="pt-10 grid  md:grid-cols-1 md:gap-6 h-20">
                 <div className="relative z-0 w-full mb-5 group">
                   <input 
                   onChange={(e) => setPhone(e.target.value)} 
                   value={phone}
                   required                  
-                  type="tel" pattern="[0-9]{10}" name="floating_phone" id="floating_phone" className="h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                  type="tel" pattern="[0-9]{10}" name="floating_phone" id="floating_phone" className="h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                   <label form="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (+91-1234567890)</label>
-                </div>
-                <div className="relative z-0 w-full h-20 mb-5 group">
-                  <input 
-                placeholder=" "
-                onChange={(e) => setCompany(e.target.value)} 
-                value={company}                 
-                  type="text" name="floating_company" id="floating_company" className="h-20 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-emerald-400 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                  <label form="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-emerald-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
                 </div>
               </div>
               <div className="relative px-10 mt-[5rem] z-0 w-full">
